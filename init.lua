8f131a920cf523ddd09f5230674b40d1c020be69
Shihab Mahamud <shihab4t@gmail.com>
2023-10-12 05:17:14 +0600

    Updates

--- init.lua
+++ init.lua
@@ -4,4 +4,6 @@ vim.o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
 
 -- Disable linting for missing doc comments in PHP
-vim.g.syntastic_php_phpcs_args = "--standard=Squiz --sniffs=PHPDocFormatting --ignore=PHPCompatibility.PHP.NewFunctions.PHP80.ltrim_whitespace PHPCompatibility.PHP.NewFunctions.PHP80.ltrim_whitespace PHPCompatibility.PHP.NewFunctions.PHP80.ltrim_whitespace"
+-- vim.g.syntastic_php_phpcs_args = "--standard=Squiz --sniffs=PHPDocFormatting --ignore=PHPCompatibility.PHP.NewFunctions.PHP80.ltrim_whitespace PHPCompatibility.PHP.NewFunctions.PHP80.ltrim_whitespace PHPCompatibility.PHP.NewFunctions.PHP80.ltrim_whitespace"
+
+

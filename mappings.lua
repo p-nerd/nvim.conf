7370aa63239f381eda526275f0fd9a8730e99dbf
Shihab Mahamud <shihab4t@gmail.com>
2023-10-19 02:10:13 +0600

    gopher nvim plugin setup for go

--- mappings.lua
+++ mappings.lua
@@ -40,4 +40,19 @@ M.dap_go = {
   }
 }
 
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags"
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    }
+  }
+}
+
 return M
--- plugins.lua
+++ plugins.lua
@@ -45,15 +45,6 @@ local plugins = {
         end
     },
     {
-        "dreamsofcode-io/nvim-dap-go",
-        ft = "go",
-        dependencies = "mfussenegger/nvim-dap",
-        config = function(_, opts)
-            require("dap-go").setup(opts)
-            require("core.utils").load_mappings("dap_go")
-        end
-    },
-    {
         "rcarriga/nvim-dap-ui",
         event = "VeryLazy",
         dependencies = "mfussenegger/nvim-dap",
@@ -91,5 +82,27 @@ local plugins = {
             },
         },
     },
+    -- Go
+    {
+        "dreamsofcode-io/nvim-dap-go",
+        ft = "go",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function(_, opts)
+            require("dap-go").setup(opts)
+            require("core.utils").load_mappings("dap_go")
+        end
+    },
+    {
+        "olexsmir/gopher.nvim",
+        ft = "go",
+        config = function(_, opts)
+            require("gopher").setup(opts)
+            require("core.utils").load_mappings("gopher")
+        end,
+        build = function()
+            vim.cmd [[silent! GoInstallDeps]]
+        end,
+    }
+
 }
 return plugins
